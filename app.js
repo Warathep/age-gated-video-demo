3e440e28faf374aa6e6b8aa7f6d37bdf9c27fb8e
Warathep Padungtin <48309588+Warathep@users.noreply.github.com>
2025-07-26 18:17:12 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+let age = prompt("กรุณากรอกอายุของคุณ")
+if(age > 13){
+    document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Qh25PD9APDM\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+}
